9831e6100b36c85ad25ba2b4b5d9a8ff3b44c4b8
aowens04 <amayowens48@gmail.com>
2026-05-12 09:34:01 -0400

    update locally hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,2 @@
-print("Hello from my local computer!")
+print("Hello from VS Code!")
 print("This version was edited locally")
-print("Hello from GitHub!")
-print("This version was edited on GitHub")
-EOF
